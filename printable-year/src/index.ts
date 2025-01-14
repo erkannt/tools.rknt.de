a6122d93a54647dab13dc6330fbf1cf7696bd758
Daniel Haarhoff <daniel@rknt.de>
2025-01-14 15:01:56 +0000

    Use data attributes to place days into columns

--- printable-year/src/index.ts
+++ printable-year/src/index.ts
@@ -16,18 +16,25 @@ const datesInYear = () => {
 };
 
 const calendarContainer = document.createElement('div');
+calendarContainer.id = 'calendarContainer';
 appContainer.appendChild(calendarContainer);
 
 const monthFormatter = new Intl.DateTimeFormat('default', { month: 'long' });
 
+let monthName: string;
 datesInYear().forEach((date) => {
   if (date.getDate() === 1) {
-    const month = document.createElement('h2');
-    month.textContent = monthFormatter.format(date);
+    const month = document.createElement('span');
+    monthName = monthFormatter.format(date);
+    month.textContent = monthName;
+    month.classList.add('month', 'calendarItem');
+    month.setAttribute('data-month', (date.getMonth() + 1).toString());
     calendarContainer.appendChild(month);
   }
 
   const day = document.createElement('div');
   day.textContent = date.toDateString();
+  day.classList.add('day', 'calendarItem');
+  day.setAttribute('data-month', (date.getMonth() + 1).toString());
   calendarContainer.appendChild(day);
 });
