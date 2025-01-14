a580d1e4f78ccfe29a2480f1ef46a258cb9bfdc9
Daniel Haarhoff <daniel@rknt.de>
2025-01-14 16:53:57 +0000

    Make year selectable

--- printable-year/src/index.ts
+++ printable-year/src/index.ts
@@ -3,42 +3,57 @@ if (!appContainer) {
   throw new Error('ERROR: no appContainer found');
 }
 
-const datesInYear = () => {
+const yearInput = document.querySelector('#yearInput');
+if (!yearInput) {
+  throw new Error('ERROR: no yearInput found');
+}
+
+const datesInYear = (year: number) => {
   const dates: Array<Date> = [];
-  let currentDate = new Date('2025');
-  console.log(currentDate.getFullYear());
-  while (currentDate.getFullYear() == 2025) {
-    console.log(currentDate);
+  let currentDate = new Date(year.toString());
+  while (currentDate.getFullYear() == year) {
     dates.push(new Date(currentDate));
     currentDate.setDate(currentDate.getDate() + 1);
   }
   return dates;
 };
 
-const calendarContainer = document.createElement('div');
-calendarContainer.id = 'calendarContainer';
-appContainer.appendChild(calendarContainer);
+const refreshCalendar = (year: number) => {
+  appContainer.innerHTML = '';
+  const calendarContainer = document.createElement('div');
+  calendarContainer.id = 'calendarContainer';
+  appContainer.appendChild(calendarContainer);
 
-const monthFormatter = new Intl.DateTimeFormat('default', { month: 'long' });
-const dayFormatter = new Intl.DateTimeFormat('default', { day: '2-digit' });
-const weekdayFormatter = new Intl.DateTimeFormat('default', { weekday: 'narrow' });
+  const monthFormatter = new Intl.DateTimeFormat('default', { month: 'long' });
+  const dayFormatter = new Intl.DateTimeFormat('default', { day: '2-digit' });
+  const weekdayFormatter = new Intl.DateTimeFormat('default', { weekday: 'narrow' });
 
-let monthName: string;
-datesInYear().forEach((date) => {
-  if (date.getDate() === 1) {
-    const month = document.createElement('span');
-    monthName = monthFormatter.format(date);
-    month.textContent = monthName;
-    month.classList.add('month', 'calendarItem');
-    month.setAttribute('data-month', (date.getMonth() + 1).toString());
-    calendarContainer.appendChild(month);
-  }
+  let monthName: string;
+  datesInYear(year).forEach((date) => {
+    if (date.getDate() === 1) {
+      const month = document.createElement('span');
+      monthName = monthFormatter.format(date);
+      month.textContent = monthName;
+      month.classList.add('month', 'calendarItem');
+      month.setAttribute('data-month', (date.getMonth() + 1).toString());
+      calendarContainer.appendChild(month);
+    }
 
-  const day = document.createElement('div');
-  day.textContent = `${dayFormatter.format(date)} ${weekdayFormatter.format(date)}`;
-  day.classList.add('day', 'calendarItem');
-  day.setAttribute('data-month', (date.getMonth() + 1).toString());
-  day.setAttribute('data-dayofweek', date.getDay().toString());
-  day.setAttribute('data-day', date.getDate().toString());
-  calendarContainer.appendChild(day);
+    const day = document.createElement('div');
+    day.textContent = `${dayFormatter.format(date)} ${weekdayFormatter.format(date)}`;
+    day.classList.add('day', 'calendarItem');
+    day.setAttribute('data-month', (date.getMonth() + 1).toString());
+    day.setAttribute('data-dayofweek', date.getDay().toString());
+    day.setAttribute('data-day', date.getDate().toString());
+    calendarContainer.appendChild(day);
+  });
+};
+
+yearInput.addEventListener('change', () => {
+  refreshCalendar(yearInput.value);
 });
+
+yearInput.value = new Date().getFullYear();
+
+console.log(yearInput.value);
+refreshCalendar(yearInput.value);
