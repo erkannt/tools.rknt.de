2d1a6499cd1dca70cd7665c6bf7ef050ae2a6616
Daniel Haarhoff <daniel@rknt.de>
2025-01-14 14:19:51 +0000

    Generate all days in a year

--- printable-year/src/index.ts
+++ printable-year/src/index.ts
@@ -1,4 +1,26 @@
 const appContainer = document.querySelector('#appContainer');
-if (appContainer) {
-  appContainer.textContent = 'Hello';
+if (!appContainer) {
+  throw new Error('ERROR: no appContainer found');
 }
+
+const datesInYear = () => {
+  const dates: Array<Date> = [];
+  let currentDate = new Date('2025');
+  console.log(currentDate.getFullYear());
+  while (currentDate.getFullYear() == 2025) {
+    console.log(currentDate);
+    dates.push(new Date(currentDate));
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+  return dates;
+};
+
+const calendarContainer = document.createElement('div');
+appContainer.appendChild(calendarContainer);
+
+datesInYear().forEach((date) => {
+  const day = document.createElement('div');
+  day.textContent = date.toDateString();
+
+  calendarContainer.appendChild(day);
+});
