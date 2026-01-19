12ae6b9fc1d42d12ad9b74c78c8ba9679930081d
Daniel Haarhoff <daniel@rknt.de>
2026-01-19 09:20:04 +0000

    PicoCSS and title

--- goldcards-log/src/routes/+layout.svelte
+++ goldcards-log/src/routes/+layout.svelte
@@ -6,6 +6,19 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>Goldcard Log</title>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.amber.min.css"
+	/>
 </svelte:head>
 
-{@render children()}
+<main class="container">
+	{@render children()}
+</main>
+
+<style>
+	main {
+		margin-top: 6rem;
+	}
+</style>
