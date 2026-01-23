1e62e223d565dc6d378e11f16152beb5458527f5
Daniel Haarhoff <daniel@rknt.de>
2026-01-23 15:40:15 +0000

    Fix styling for years with 53 weeks

--- chunked-calendar/src/lib/Calendar.svelte
+++ chunked-calendar/src/lib/Calendar.svelte
@@ -17,13 +17,17 @@
 <div class="calendar-grid">
     {#each quarters as quarter}
         <div class="quarter">
-            {#each weekdays as wd}
-                <div class="weekday">{wd}</div>
-            {/each}
-            {#each quarter as week}
-                {#each week as day}
-                    <div>{formatDay(day)}</div>
+            <div class="weekday-row">
+                {#each weekdays as wd}
+                    <div class="weekday">{wd}</div>
                 {/each}
+            </div>
+            {#each quarter as week}
+                <div class="week">
+                    {#each week as day}
+                        <div class="day">{formatDay(day)}</div>
+                    {/each}
+                </div>
             {/each}
         </div>
     {/each}
@@ -37,21 +41,34 @@
     }
 
     .quarter {
-        display: grid;
-        grid-template-columns: repeat(7, 1fr);
+        display: flex;
+        flex-direction: column;
+        gap: var(--space-2xs);
         text-align: center;
-        row-gap: var(--space-2xs);
     }
 
-    /* larger gap after 5th, 9th, and 13th rows */
-    .quarter > :nth-child(35),
-    .quarter > :nth-child(63),
-    .quarter > :nth-child(91) {
-        margin-bottom: var(--space-m);
+    .weekday-row,
+    .week {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    /* Add margin‑bottom to the 5th, 9th, and 13th week rows */
+    .quarter .week:nth-child(5),
+    .quarter .week:nth-child(9),
+    .quarter .week:nth-child(13) {
+        margin-bottom: var(--space-xs);
     }
 
     .weekday {
         font-weight: bold;
         border-bottom: 3px solid black;
+        flex: 1;
+        text-align: center;
+    }
+
+    .day {
+        flex: 1;
+        text-align: center;
     }
 </style>
