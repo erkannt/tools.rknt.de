cee7fb5f90ff192cfbcf0be01052db79085ac233
Daniel Haarhoff <daniel@rknt.de>
2026-01-23 14:49:19 +0000

    Style the year input

--- chunked-calendar/src/App.svelte
+++ chunked-calendar/src/App.svelte
@@ -2,17 +2,39 @@
     import Calendar from "./lib/Calendar.svelte";
     const thisYear = () => new Date().getFullYear();
 
-    const year = $state(thisYear());
+    let year = $state(thisYear());
 </script>
 
 <main>
-    <h1>Chunked {year}</h1>
+    <h1><span>Chunked</span><input type="number" bind:value={year} /></h1>
 
-    <p>Simply print this page.</p>
-    <p>
-        Inspired by <a href="https://www.youtube.com/watch?v=BiY2yUwTgQc"
-            >a JashiiCorrin video</a
-        >.
-    </p>
+    <section>
+        <p>Simply print this page.</p>
+        <p>
+            Inspired by <a href="https://www.youtube.com/watch?v=BiY2yUwTgQc"
+                >a JashiiCorrin video</a
+            >.
+        </p>
+    </section>
     <Calendar />
 </main>
+
+<style>
+    h1 {
+        display: flex;
+        align-items: baseline;
+        gap: var(--space-s);
+    }
+
+    h1 input {
+        border: 3px solid currentColor;
+        font-size: inherit;
+        appearance: textfield;
+        width: 5ch;
+        text-align: center;
+    }
+
+    section {
+        margin-bottom: var(--space-xl);
+    }
+</style>
