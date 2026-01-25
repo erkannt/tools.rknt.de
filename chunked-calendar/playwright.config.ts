5f398a2cb6db95f68d4ee27f425ba08edf29738e
Daniel Haarhoff <daniel@rknt.de>
2026-01-25 20:06:32 +0000

    run browsers inside of container for consistent results

--- chunked-calendar/playwright.config.ts
+++ chunked-calendar/playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: "dot",
   use: {
-    baseURL: "http://localhost:5173",
+    baseURL: process.env.BASE_URL || "http://localhost:5173",
     trace: "on-first-retry",
     screenshot: "only-on-failure",
   },
@@ -36,8 +36,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: "pnpm vite --port 5173",
+    command: "npx vite --port 5173",
     port: 5173,
-    reuseExistingServer: !process.env.CI,
   },
 });
