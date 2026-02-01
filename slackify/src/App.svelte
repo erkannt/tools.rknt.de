0875a7f3febd4d010bb389db9f4666b2007a920a
Daniel Haarhoff <daniel@rknt.de>
2026-02-01 12:17:20 +0000

    Add more structure

--- slackify/src/App.svelte
+++ slackify/src/App.svelte
@@ -17,17 +17,30 @@
     </header>
 
     <section>
-        <textarea
-            bind:value={markdown}
-            rows="6"
-            placeholder="Enter markdown here..."
-        ></textarea>
-
         <button onclick={clear}> Clear </button>
     </section>
 
     <section>
+        <h2>Markdown</h2>
+        <textarea
+            bind:value={markdown}
+            rows="12"
+            placeholder="Enter markdown here..."
+        ></textarea>
+    </section>
+
+    <section id="slackified-output">
         <h2>Slackified</h2>
         <pre>{slackified}</pre>
     </section>
 </main>
+
+<style>
+    #slackified-output {
+        pre {
+            border: 1px solid black;
+            min-height: 20rem;
+            overflow: scroll;
+        }
+    }
+</style>
