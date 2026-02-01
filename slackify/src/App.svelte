cc2251e2c30e31860e3a4f066e73faf2e9d11815
Daniel Haarhoff <daniel@rknt.de>
2026-02-01 10:12:22 +0000

    Add input for markdown with clear button

--- slackify/src/App.svelte
+++ slackify/src/App.svelte
@@ -1,4 +1,23 @@
+<script lang="ts">
+    let markdown = $state("");
+    const clear = () => {
+        markdown = "";
+    };
+</script>
+
 <main>
-    <h1>Slackify</h1>
-    <p>Convert Markdown to Slack markup ready for pasting.</p>
+    <header>
+        <h1>Slackify</h1>
+        <p>Convert Markdown to Slack markup ready for pasting.</p>
+    </header>
+
+    <section>
+        <textarea
+            bind:value={markdown}
+            rows="6"
+            placeholder="Enter markdown here..."
+        ></textarea>
+
+        <button onclick={clear}> Clear </button>
+    </section>
 </main>
