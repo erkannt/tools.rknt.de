c2f8cdb58a9fd987f5613d85257f42489a05241f
Daniel Haarhoff <daniel@rknt.de>
2026-02-04 19:52:43 +0000

    Start building snippets

---
+++ snippets/src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import "../../tools-landing/styles/main.css";
+
+    function handleClick() {
+        navigator.clipboard
+            .writeText("foobar")
+            .then(() => {
+                window.close();
+            })
+            .catch((err) => {
+                console.error("Failed to copy to clipboard:", err);
+            });
+    }
+</script>
+
+<main>
+    <h1>Snippets</h1>
+    <button on:click={handleClick}>Copy foobar</button>
+</main>
