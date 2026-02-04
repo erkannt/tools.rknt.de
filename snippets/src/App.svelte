c6c473b83993d5093f802ae9c65a7707e508a74e
Daniel Haarhoff <daniel@rknt.de>
2026-02-04 19:58:03 +0000

    Add form for snippet input

--- snippets/src/App.svelte
+++ snippets/src/App.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
     import "../../tools-landing/styles/main.css";
 
+    type Snippet = {
+        id: string;
+        title: string;
+        content: string;
+    };
+
+    var snippets: Snippet[] = [];
+
     function handleClick() {
         navigator.clipboard
             .writeText("foobar")
@@ -16,4 +24,33 @@
 <main>
     <h1>Snippets</h1>
     <button on:click={handleClick}>Copy foobar</button>
+    <form>
+        <label for="title">Title:</label>
+        <input type="text" id="title" name="title" required />
+
+        <label for="content">Content:</label>
+        <textarea id="content" name="content" rows="5" required></textarea>
+
+        <button type="submit">Add Snippet</button>
+    </form>
 </main>
+
+<style>
+    form {
+        display: flex;
+        flex-direction: column;
+
+        label {
+            margin-block-end: var(--space-2xs);
+        }
+
+        input,
+        textarea {
+            margin-block-end: var(--space-s);
+        }
+
+        button {
+            padding-block: var(--space-2xs);
+        }
+    }
+</style>
