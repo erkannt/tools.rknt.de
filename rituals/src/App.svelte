38699700a34ac94a98974190132db7bc46d86329
Daniel Haarhoff <daniel@rknt.de>
2026-02-21 20:44:13 +0000

    Allow viewing rituals

--- rituals/src/App.svelte
+++ rituals/src/App.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { marked } from "marked";
+
   interface Ritual {
     id: number;
     name: string;
@@ -6,13 +8,24 @@
   }
 
   let rituals: Ritual[] = $state([]);
-  let showForm = $state(false);
+  let view: "home" | "add" | "view" = $state("home");
+  let currentRitual: Ritual | null = $state(null);
   let name = $state("");
   let markdown = $state("");
   let nextId = $state(1);
 
-  function addRitual() {
-    showForm = true;
+  function goToAdd() {
+    view = "add";
+  }
+
+  function goToHome() {
+    view = "home";
+    currentRitual = null;
+  }
+
+  function viewRitual(ritual: Ritual) {
+    currentRitual = ritual;
+    view = "view";
   }
 
   function saveRitual() {
@@ -20,14 +33,20 @@
     nextId += 1;
     name = "";
     markdown = "";
-    showForm = false;
+    view = "home";
+  }
+
+  function renderMarkdown(content: string): string {
+    return marked.parse(content) as string;
   }
 </script>
 
 <main>
-  <h1>Rituals</h1>
+  {#if view !== "view"}
+    <h1>Rituals</h1>
+  {/if}
 
-  {#if showForm}
+  {#if view === "add"}
     <form
       onsubmit={(e) => {
         e.preventDefault();
@@ -45,17 +64,21 @@
       </div>
       <button type="submit">Save</button>
     </form>
+  {:else if view === "view" && currentRitual}
+    <a href="#" onclick={goToHome}>Back to Home</a>
+    <h1>{currentRitual.name}</h1>
+    <div>{@html renderMarkdown(currentRitual.markdown)}</div>
   {:else}
-    <button onclick={addRitual}>Add Ritual</button>
-
     {#if rituals.length > 0}
       <ul>
         {#each rituals as ritual (ritual.id)}
           <li>
-            <a href="#">{ritual.name}</a>
+            <a href="#" onclick={() => viewRitual(ritual)}>{ritual.name}</a>
           </li>
         {/each}
       </ul>
     {/if}
+
+    <button onclick={goToAdd}>Add Ritual</button>
   {/if}
 </main>
