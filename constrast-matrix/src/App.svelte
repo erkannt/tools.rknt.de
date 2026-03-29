c9a25d994e44ae97387f8beaee50a7d417cb07e7
Daniel Haarhoff <daniel@rknt.de>
2026-03-29 11:50:38 +0100

    Init contrast matrix and sketch out shape

---
+++ constrast-matrix/src/App.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  const exampleInput = `
+  --red: #cc4c19;
+  --green: #365a31;
+  --blue: #3279bc;
+  --darkblue: #263e45;
+  --yellow: #d19815;
+  --cyan: #28bfce;
+
+  --white: #fdf9f0; /* Warm off-white, inspired by yellow */
+  --black: #1e2e34; /* Darker, richer variant of darkblue */
+`;
+</script>
+
+<h1>Contrast Matrix</h1>
+<form>
+  <textarea> </textarea>
+  <button type="submit">Import</button>
+</form>
+
+<h2>Your Colours</h2>
+
+<h2>Possible combinations</h2>
+
+<h3>AAA Normal</h3>
+<ul>
+  <li>Black on White</li>
+</ul>
+
+<button>Copy AAA Normal utility classes</button>
+
+<h3>AA Normal</h3>
+<h3>AAA Large</h3>
+<h3>AA Large</h3>
+
+<h2>Insufficient Contrast</h2>
