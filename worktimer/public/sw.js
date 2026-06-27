78b9369549ea4e7e09a96c104e2f107170fb899d
Daniel Haarhoff <daniel@rknt.de>
2026-06-27 15:15:50 +0100

    worktimer: fix caching strategy so that PWA updates itself

--- worktimer/public/sw.js
+++ worktimer/public/sw.js
@@ -33,22 +33,15 @@ self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
 
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-
-      return fetch(event.request).then((response) => {
-        if (!response || response.status !== 200 || response.type !== 'basic') {
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request).then((cachedResponse) => {
+        const fetchPromise = fetch(event.request).then((response) => {
+          if (response && response.status === 200 && response.type === 'basic') {
+            cache.put(event.request, response.clone());
+          }
           return response;
-        }
-
-        const responseToCache = response.clone();
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, responseToCache);
         });
-
-        return response;
+        return cachedResponse || fetchPromise;
       });
     })
   );
