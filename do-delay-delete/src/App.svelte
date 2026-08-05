ab63a7698458586215f49a8c91da91349f161557
Daniel Haarhoff <daniel@rknt.de>
2026-08-05 19:44:10 +0100

    ddd: sketch ui

--- do-delay-delete/src/App.svelte
+++ do-delay-delete/src/App.svelte
@@ -1,1 +1,27 @@
-<h1>DoDelayDelete</h1>
+<h1>do delay delete</h1>
+
+<ul>
+  <li>
+    <p>buy milk</p>
+    <button>done</button>
+    <button>delay</button>
+    <button>delete</button>
+  </li>
+  <li>
+    <p>go for run</p>
+    <button>done</button>
+    <button>delay</button>
+    <button>delete</button>
+  </li>
+  <li>
+    <p>fix bike</p>
+    <button>done</button>
+    <button>delay</button>
+    <button>delete</button>
+  </li>
+</ul>
+
+<form action="">
+  <textarea name="" id=""></textarea>
+  <button>create action</button>
+</form>
